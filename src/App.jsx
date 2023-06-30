a4219fc3d9dcb101557b500b7bb95f08be575699
Shiv Nandan Jha <neureto12@gmail.com>
2023-06-30 14:10:17 +0530

    Final Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // import { useState } from 'react'
 import './App.css'
-import PodcastPage from './components/PodcastPage';
+import PodcastPage from '../src/components/PodcastPage';
 
 function App() {
   return (
